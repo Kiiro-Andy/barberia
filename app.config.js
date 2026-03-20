3b27beb90aeb2c0349736bff5186dd9b2fe45219
Roberto <elrobert741@gmail.com>
2026-03-19 17:23:08 -0700

    a?

---
+++ app.config.js
@@ -0,0 +1,51 @@
+import 'dotenv/config'; // Esto carga tu .env local en desarrollo
+
+export default {
+  expo: {
+    name: "barbershop",
+    slug: "barbershop",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/logo.png",
+    userInterfaceStyle: "light",
+    newArchEnabled: true,
+    notification: {
+      icon: "./assets/logo.png",
+      color: "#000000",
+      iosDisplayInForeground: true
+    },
+    splash: {
+      image: "./assets/logo.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff"
+    },
+    ios: {
+      supportsTablet: true
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./assets/logo.png",
+        backgroundColor: "#ffffff"
+      },
+      googleServicesFile: "./google-services.json",
+      edgeToEdgeEnabled: true,
+      package: "com.elrobertds.barbershop"
+    },
+    web: {
+      favicon: "./assets/logo.png"
+    },
+    plugins: [
+      "expo-asset",
+      "expo-font",
+      "expo-notifications",
+      "@react-native-community/datetimepicker"
+    ],
+    extra: {
+      eas: {
+        projectId: "034715c3-bb2e-48e4-b540-6eb0bd57079b"
+      },
+      supabaseUrl: process.env.SUPABASE_URL,
+      supabaseAnonKey: process.env.SUPABASE_ANON_KEY
+    }
+  }
+};
